ad3d40bccf4d295cba967394b00f145f8539d3f0
Shihab0 <ataharshihab1@gmail.com>
2022-11-17 08:54:57 +0600

    date query set

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// DB_USER=doctorsPortal
-// DB_PASSWORD=Dw17TuH3Ivt52Rog
-
 //////////// MongoDB connection ///////////
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ueibnfi.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -36,19 +33,47 @@ dbConnect();
 const appointmentOptionCollection = client
   .db("doctorsPortal")
   .collection("appointmentOption");
-/////////////// ///////////////////////
 
+const bookingsCollection = client.db("doctorsPortal").collection("bookings");
+
+/////////////// ///////////////////////
 ////////////// appointment option get post ///////////////
 
 app.get("/appointmentOption", async (req, res) => {
   try {
+    const date = req.query.date;
     const query = {};
     const options = await appointmentOptionCollection.find(query).toArray();
+    const bookingQuery = { appointmentDate: date };
+    const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+    options.forEach((option) => {
+      const optionBooked = alreadyBooked.filter(
+        (book) => book.treatment === option.name
+      );
+      const bookedSlots = optionBooked.map((book) => book.slot);
+      // console.log(option.name, bookedSlots);
+    });
     res.send(options);
   } catch (err) {
     console.log(err);
   }
 });
+
+//////////////////////////////////////////
+//////////////bookings option get post etc ///////////////
+
+app.post("/bookings", async (req, res) => {
+  try {
+    const booking = req.body;
+    const result = await bookingsCollection.insertOne(booking);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+///////////////////////////
 
 app.get("/", (req, res) => {
   res.send("doctors portal server is running");
